ad2a38ed45308bd5cb49305b5198c2146456144e
john <john.flouis1@gmail.com>
2022-12-12 16:23:38 +0200

    handle GET requests

--- app/app.js
+++ app/app.js
@@ -10,8 +10,67 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// GET requests
+app.get('/registration', function(req,res){
+  res.render('registration')
+});
+
+app.get('/login', function(req,res){
+  res.render('login')
+});
+
 app.get('/', function(req,res){
-  res.render('index', {title: "express"})
+  res.render('login')
+});
+
+app.get('/home', function(req,res){
+  res.render('home')
+});
+
+app.get('/searchresults', function(req,res){
+  res.render('searchresults')
+});
+
+app.get('/novel', function(req,res){
+  res.render('novel')
+});
+
+app.get('/fiction', function(req,res){
+  res.render('fiction')
+});
+
+app.get('/poetry', function(req,res){
+  res.render('poetry')
+});
+
+app.get('/readlist', function(req,res){
+  res.render('readlist')
+});
+
+app.get('/dune', function(req,res){
+  res.render('dune')
+});
+
+app.get('/flies', function(req,res){
+  res.render('flies')
+});
+
+app.get('/grapes', function(req,res){
+  res.render('grapes')
+});
+
+app.get('/leaves', function(req,res){
+  res.render('leaves')
+});
+
+app.get('/mockingbird', function(req,res){
+  res.render('mockingbird')
+});
+
+app.get('/sun', function(req,res){
+  res.render('sun')
 });
 
 module.exports = app;
+
+app.listen(3000);
